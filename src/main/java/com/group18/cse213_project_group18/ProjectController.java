268e400945969519e9004c7dbe53543cbc3ec22c
rafi <2222222@iub.edu.bd>
2025-11-11 12:08:02 +0600

    second

---
+++ src/main/java/com/group18/cse213_project_group18/ProjectController.java
@@ -0,0 +1,7 @@
+package com.group18.cse213_project_group18;
+
+public class ProjectController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ src/main/java/com/group18/cse213_project_group18/adiba/ProjecttController.java
@@ -0,0 +1,7 @@
+package com.group18.cse213_project_group18.adiba;
+
+public class ProjecttController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
